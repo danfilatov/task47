12dcbe20a08e517f0cd6581a1373ac8735876bfe
Denis Filatov <gooden@mail.ru>
2023-03-20 19:27:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] numArray = new double[m, n];
+Random numRandom = new Random();
+
+void PrintArray(double[,] matrix)
+ { 
+    for (int i = 0; i < m; i++)
+    { 
+        for (int j = 0; j < n; j++)
+       { 
+          Console.Write($"{matrix[i, j]}; ");
+       }
+ Console.WriteLine();
+    }
+ }
+
+void FillArray(double[,] matrix)
+ { 
+    for (int i = 0; i < m; i++)
+    { 
+      for (int j = 0; j < n; j++)
+       { 
+          matrix[i,j] = Convert.ToDouble(numRandom.Next(-10, 10)/10.0);
+       }
+    }
+}
+FillArray(numArray);
+PrintArray(numArray);
